f9d04d2bb1f5d36877c96892ce868421ac275e57
Sridhar-Ghr <sridhar.cisms@gmail.com>
2025-10-15 10:45:44 +0530

    created development branch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI demo!");
+        System.out.println("Added this statement when Development branch is creatd");
     }
 }
